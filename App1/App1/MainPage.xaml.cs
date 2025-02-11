769cba0fd091824b72ebfb18376c3ba1e5ad6750
pl1313 <pl1313@203N11>
2025-02-11 15:42:56 +0500

    Добавьте файлы проекта.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ App1/App1/Pass.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pass : ContentPage
+    {
+
+        Label textLabel;
+        Entry loginEntry, passwordEntry;
+        public Pass()
+        {
+
+
+            StackLayout stackLayout = new StackLayout();
+
+            loginEntry = new Entry { Placeholder = "Login" };
+            loginEntry.TextChanged += loginEntry_TextChanged;
+
+            passwordEntry = new Entry
+            {
+                Placeholder = "Password",
+                IsPassword = true
+            };
+            textLabel = new Label { FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) };
+
+            stackLayout.Children.Add(loginEntry);
+            stackLayout.Children.Add(passwordEntry);
+            stackLayout.Children.Add(textLabel);
+            this.Content = stackLayout;
+        }
+
+        private void loginEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textLabel.Text = loginEntry.Text;
+        }
+    }
+}
